4b12ce17a99231e22b2c84681d01a95f7bb5ca6a
Vincent Van Ghelue <root@tessidev.tessi>
2013-06-26 09:59:18 +0200

    dependency injection fixes

--- DependencyInjection/VVGFilterListExtension.php
+++ DependencyInjection/VVGFilterListExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class FilterlistExtension extends Extension
+class VVGFilterListExtension extends Extension
 {
     /**
      * {@inheritDoc}
